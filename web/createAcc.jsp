d0d84ebda54fbb04f6023c6b952145b329abd127
madsbyday <madsbyday@gmail.com>
2017-05-03 09:44:14 +0200

    Made some validation on the createAacc site

--- web/createAcc.jsp
+++ web/createAcc.jsp
@@ -10,18 +10,20 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link href="WebStyle.css" rel="stylesheet" type="text/css"/>
+        <script src="jquery-3.2.1.min.js" type="text/javascript"></script>
+        <script src="validation.js" type="text/javascript"></script>
         <title>Create Account</title>
     </head>
     <body>
         <header><h1>Johannes Fog - Carport Builder</h1></header>
         <div id="container">
-            <form action="SLcreate" method="post" name="createuser">
-                <h3>Username:</h3><input class="login" type="text" name="username" placeholder="user1234">
-                <h3>Password:</h3><input class="login" type="text" name="password" placeholder="6-12 characters">
-                <h3>Address:</h3><input class="login" type="text" name="address" placeholder="Road street 1">
-                <h3>Zipcode:</h3><input class="login" type="text" name="zipcode" placeholder="0000">
-                <h3>Phone:</h3><input class="login" type="text" name="phone" placeholder="00000000">
-                <h3>Email:</h3><input class="login" type="text" name="email" placeholder="yourmail@mail.com">
+            <form action="SLcreate" method="post" name="createuser" onsubmit="return validateForm()">
+                <h3>Username:</h3><input class="login" id="inName" type="text" name="username" placeholder="user1234">
+                <h3>Password:</h3><input class="login" id="inPass" type="text" name="password" placeholder="6-12 characters">
+                <h3>Address:</h3><input class="login" id="inAdd" type="text" name="address" placeholder="Road street 1">
+                <h3>Zipcode:</h3><input class="login" id="inZip" type="text" name="zipcode" placeholder="0000">
+                <h3>Phone:</h3><input class="login" id="inPhone" type="text" name="phone" placeholder="00000000">
+                <h3>Email:</h3><input class="login" id="inMail" type="text" name="email" placeholder="yourmail@mail.com">
                 <input id="button" type="submit" value="Create" />
             </form>
         </div>
--- web/validation.js
+++ web/validation.js
@@ -1,7 +1,58 @@
 function validateForm() {
     var name = document.forms["createuser"]["username"].value;
-    if (name == "") {
-        alert("username must be filled out");
+    var password = document.forms["createuser"]["password"].value;
+    var address = document.forms["createuser"]["address"].value;
+    var zip = document.forms["createuser"]["zipcode"].value;
+    var phone = document.forms["createuser"]["phone"].value;
+    var mail = document.forms["createuser"]["email"].value;
+
+    if (name === "" || name.match(' ') || password === "" || password.match(' ') || address == "" || zip == "" || zip.match(' ') || phone == "" || phone.match(' ') || phone.length != 8 || mail == "" || mail.match(' '))
+    {
+        if (name === "" || name.match(' ')) {
+            $("#inName").css("border", "2px solid red");
+        }
+        if (password === "" || password.match(' ')) {
+            $("#inPass").css("border", "2px solid red");
+        }
+        if (address == "") {
+            $("#inAdd").css("border", "2px solid red");
+        }
+        if (zip == "" || zip.match(' ')) {
+            $("#inZip").css("border", "2px solid red");
+        }
+        if (phone == "" || phone.match(' ') || phone.length != 8) {
+            $("#inPhone").css("border", "2px solid red");
+        }
+        if (mail == "" || mail.match(' ')) {
+            $("#inMail").css("border", "2px solid red");
+        }
+        
+        
+        
+        
+        
+        
+        if (!(name === "" || name.match(' '))) {
+            $("#inName").css("border", "2px solid green");
+        }
+        if (!(password === "" || password.match(' '))) {
+            $("#inPass").css("border", "2px solid green");
+        }
+        if (!(address == "")) {
+            $("#inAdd").css("border", "2px solid green");
+        }
+        if (!(zip == "" || zip.match(' '))) {
+            $("#inZip").css("border", "2px solid green");
+        }
+        if (!(phone == "" || phone.match(' ') || phone.length != 8)) {
+            $("#inPhone").css("border", "2px solid green");
+        }
+        if (!(mail == "" || mail.match(' '))) {
+            $("#inMail").css("border", "2px solid green");
+        }
         return false;
+    }
+    else {
+        return true;
     }
 }
