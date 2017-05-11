75127a2f40e915a1ab41e2e84626497c0effff8e
madsbyday <madsbyday@gmail.com>
2017-05-11 10:56:55 +0200

    Impl calculator og roofs

--- src/java/result/Calculator.java
+++ src/java/result/Calculator.java
@@ -93,7 +93,74 @@ public class Calculator {
                     list.get(i).setLength(l.);
                 }
             } else if (skur == false) {
-
+                // id 17
+                if (i == 1) {
+                    list.get(i).setLength(l.vindskeder(width, angle));
+                    list.get(i).setAmount(l.vindskedeAntal(width, angle));
+                }
+                // id 18
+                if (i == 2) {
+                    list.get(i).setLength(l.sternSide(length));
+                    list.get(i).setAmount(l.sternSideAntal());
+                }
+                // id 19 - ???
+                if (i == 3) {
+                    list.remove(i);
+                }
+                // id 20
+                if (i == 4) {
+                    list.get(i).setAmount(l.færdigByggetAntal());
+                }
+                // id 21
+                if (i == 5) {
+                    list.get(i).setLength(l.stolperLength());
+                    list.get(i).setAmount(l.stolperAntal(skur));
+                }
+                // id 22
+                if (i == 6) {
+                    list.get(i).setLength(l.remme(length));
+                    list.get(i).setAmount(l.remmeAntal());
+                }
+                // id 23
+                if (i == 7) {
+                    list.remove(i);
+                }
+                // id 24
+                if (i == 8) {
+                    list.remove(i);
+                }
+                // id 25
+                if (i == 9) {
+                    list.remove(i);
+                }
+                // id 26
+                if (i == 10) {
+                    list.get(i).setLength(l.vandbræt(width, angle));
+                    list.get(i).setAmount(l.vandbrætAntal(width, angle));
+                }
+                // id 27
+                if (i == 11) {
+                    list.get(i).setLength(l.gavlLength(width));
+                    list.get(i).setAmount(l.gavlAntal(width));
+                }
+                // id 28 - antal?
+                if (i == 12) {
+                    list.remove(i);
+                }
+                // id 29
+                if (i == 13) {
+                    list.get(i).setLength(l.brætOverTagfodslægte());
+                    list.get(i).setAmount(l.brætOverAntal());
+                }
+                // id 30
+                if (i == 14) {
+                    list.get(i).setLength(l.brætBagdør());
+                    list.get(i).setAmount(l.brætBagdørAntal());
+                }
+                // id 31
+                if (i == 15) {
+                    list.get(i).setLength(l.);
+                }
             }
         }
     }
