2b1ec0da903af1d04014900bf4f598a45599f0d1
vfgya <vfgya@DESKTOP-RVBPB65>
2017-05-28 16:38:10 +0200

    optimeret koden

--- src/java/facade/Facade.java
+++ src/java/facade/Facade.java
@@ -14,156 +14,174 @@ import java.util.ArrayList;
  *
  * @author Jonas
  */
-public class Facade implements FacadeInterface {
+public class Facade implements FacadeInterface
+{
 
     private GenerateBlueprints gb;
     private DBFacade DB;
     private Calculator calc;
 
-    public Facade() {
+    public Facade()
+    {
         this.gb = new GenerateBlueprints();
         this.DB = new DBFacade();
     }
 
     @Override
-    public String outlineTop(int height, int width) {
+    public String outlineTop(int height, int width)
+    {
         return gb.outlineTop(height, width);
     }
 
     @Override
-    public String stem(int height, int width) {
+    public String stem(int height, int width)
+    {
         return gb.stem(height, width);
     }
 
     @Override
-    public ArrayList<String> taglaegteUp(int height, int width) {
+    public ArrayList<String> taglaegteUp(int height, int width)
+    {
         return gb.taglaegteUp(height, width);
     }
 
     @Override
-    public ArrayList<String> taglaegteDown(int height, int width) {
+    public ArrayList<String> taglaegteDown(int height, int width)
+    {
         return gb.taglaegteDown(height, width);
     }
 
     @Override
-    public ArrayList<String> createSpaerFlat(int height, int width) {
+    public ArrayList<String> createSpaerFlat(int height, int width)
+    {
         return gb.createSpaerFlat(height, width);
     }
 
     @Override
-    public ArrayList<String> postsTop(int width, boolean rejsning, boolean shed) {
+    public ArrayList<String> postsTop(int width, boolean rejsning, boolean shed)
+    {
         return gb.postsTop(width, rejsning, shed);
     }
 
     @Override
-    public ArrayList<String> postsBottom(int height, int width, boolean rejsning, boolean shed) {
+    public ArrayList<String> postsBottom(int height, int width, boolean rejsning, boolean shed)
+    {
         return gb.postsBottom(height, width, rejsning, shed);
     }
 
     @Override
-    public String bjaelkeTopFlat(int width) {
+    public String bjaelkeTopFlat(int width)
+    {
         return gb.bjaelkeTopFlat(width);
     }
 
     @Override
-    public String bjaelkeBottomFlat(int width, int height) {
+    public String bjaelkeBottomFlat(int width, int height)
+    {
         return gb.bjaelkeBottomFlat(width, height);
     }
 
     @Override
-    public ArrayList<String> createSpaerRejsning(int height, int width) {
+    public ArrayList<String> createSpaerRejsning(int height, int width)
+    {
         return gb.createSpaerRejsning(height, width);
     }
 
     @Override
-    public String bjaelkeTopRejsning(int width) {
+    public String bjaelkeTopRejsning(int width)
+    {
         return gb.bjaelkeTopRejsning(width);
     }
 
     @Override
-    public String bjaelkeBottomRejsning(int width, int height) {
+    public String bjaelkeBottomRejsning(int width, int height)
+    {
         return gb.bjaelkeBottomRejsning(width, height);
     }
 
     @Override
-    public String vindkryds(int height, int width) {
+    public String vindkryds(int height, int width)
+    {
         return gb.vindkryds(height, width);
     }
 
     @Override
-    public String sideFlatRoof(int width) {
+    public String sideFlatRoof(int width)
+    {
         return gb.sideFlatRoof(width);
     }
 
     @Override
-    public String sideFlatRoof2(int width) {
+    public String sideFlatRoof2(int width)
+    {
         return gb.sideFlatRoof2(width);
     }
 
     @Override
-    public ArrayList<String> supportPosts(int width, boolean rejsning, boolean shed, int roofHeight) {
+    public ArrayList<String> supportPosts(int width, boolean rejsning, boolean shed, int roofHeight)
+    {
         return gb.supportPosts(width, rejsning, shed, roofHeight);
     }
 
     @Override
-    public ArrayList<String> getMeasurementsTop() {
+    public ArrayList<String> getMeasurementsTop()
+    {
         return gb.getMeasurementsTop();
     }
 
     @Override
-    public ArrayList<String> getMeasurementsSide() {
+    public ArrayList<String> getMeasurementsSide()
+    {
         return gb.getMeasurementsSide();
     }
 
     @Override
-    public String shedOutline(int height, int width, boolean rejsning) {
+    public String shedOutline(int height, int width, boolean rejsning)
+    {
         return gb.shedOutline(height, width, rejsning);
     }
 
     @Override
-    public String shedStolper(int height, int width, boolean rejsning) {
+    public String shedStolper(int height, int width, boolean rejsning)
+    {
         return gb.shedStolper(height, width, rejsning);
     }
 
     @Override
-    public ArrayList<String> shedSide(int width, boolean rejsning, int roofHeight) {
+    public ArrayList<String> shedSide(int width, boolean rejsning, int roofHeight)
+    {
         return gb.shedSide(width, rejsning, roofHeight);
     }
 
     @Override
-    public ArrayList<String> roofboards(int totalWidth, int roofHeight) {
+    public ArrayList<String> roofboards(int totalWidth, int roofHeight)
+    {
         return gb.roofboards(totalWidth, roofHeight);
     }
 
     @Override
-    public String roofHorizontal(int width, int roofHeight) {
+    public String roofHorizontal(int width, int roofHeight)
+    {
         return gb.roofHorizontal(width, roofHeight);
     }
 
     @Override
-    public String roofEnds(int width, int roofHeight) {
+    public String roofEnds(int width, int roofHeight)
+    {
         return gb.roofEnds(width, roofHeight);
     }
 
     @Override
-    public void calculateResultLifted(ArrayList<Material> list, int length, int width, int angle, boolean skur, int skurlength, int skurwidth) {
-        list = DB.getAllMaterials();
-        try {
-            calc.calculateResultLifted(list, length, width, angle, skur, skurlength, skurwidth);
-        } catch (Exception e) {
-            System.out.println("fejl i LiftedRoof");
-        }
+    public void calculateResultLifted(ArrayList<Material> list, int length, int width, int angle, boolean skur, int skurlength, int skurwidth)
+    {
+        calc.calculateResultLifted(list, length, width, angle, skur, skurlength, skurwidth);
 
     }
 
     @Override
-    public void calculateResultFlat(ArrayList<Material> list, int length, int width, boolean skur, int skurlength, int skurwidth) {
-        list = DB.getAllMaterials();
-        try {
-            calc.calculateResultFlat(list, length, width, skur, skurlength, skurwidth);
-        } catch (Exception e) {
-            System.out.println("fejl i FlatRoof");
-        }
+    public void calculateResultFlat(ArrayList<Material> list, int length, int width, boolean skur, int skurlength, int skurwidth)
+    {
+        calc.calculateResultFlat(list, length, width, skur, skurlength, skurwidth);
     }
 
 }
